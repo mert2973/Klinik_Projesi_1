0c32cc5ff23e259a2a833887228b77f3659aa7fe
mert2973 <31030119+mert2973@users.noreply.github.com>
2020-02-22 11:28:37 +0300

    başlık düzeltmeleri

--- resources/views/pages/Appointments.blade.php
+++ resources/views/pages/Appointments.blade.php
@@ -9,10 +9,10 @@
     <div class="page-body"><div class="page-title">
             <div class="row align-items-center">
                 <div class="col-sm-6">
-                    <h2 class="page-title-text d-inline-block">Appointments mert</h2>
+                    <h2 class="page-title-text d-inline-block">Appointments</h2>
                     <div class="breadcrumbs d-inline-block">
                         <ul>
-                            <li><a href="{{url('/Dashboard')}}">Dashboard hatay</a></li>
+                            <li><a href="{{url('/Dashboard')}}">Dashboard</a></li>
                             <li>Appointments</li>
                         </ul>
                     </div>
