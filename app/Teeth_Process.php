5820a9c883d9ee6f643eb942a422503843582514
mert2973 <31030119+mert2973@users.noreply.github.com>
2020-04-22 10:22:04 +0300

    Yeni güncellemeler 01

--- app/Teeth_Process.php
+++ app/Teeth_Process.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Teeth_Process extends Model
 {
-    //
+   protected $table="teeth_processes";
+   protected $fillable=["apt_id","prc_name","piece","exp_prc","teeth_num"];
 }
